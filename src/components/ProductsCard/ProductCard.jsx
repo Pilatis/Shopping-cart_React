000a345ece02710a8241a646bf8b4267d6e8108d
Lucas <pilatilucas@gmail.com>
2023-12-04 14:58:08 -0300

    Update ProductCard.jsx

--- src/components/ProductsCard/ProductCard.jsx
+++ src/components/ProductsCard/ProductCard.jsx
@@ -8,17 +8,18 @@ import AppContext from '../../context/AppContext';
 
 const ProductCard = ({ data }) => {
 
-  const { title, thumbnail, price, condition } = data;
-  const { cartItems, setCartItems } = useContext(AppContext);
+  const { title, thumbnail, price, condition } = data;//destruturando
+  const { cartItems, setCartItems } = useContext(AppContext);//pegando os estados dentro do AppContext
 
-  const handleAddCart = () => {
-     setCartItems([ ...cartItems, data ])
+  const handleAddCart = () => {//Define a função handleAddCart, que é chamada quando o botão de adicionar ao carrinho é clicado.
+     setCartItems([ ...cartItems, data ])//Esta função utiliza setCartItems para adicionar o data atual ao cartItems, criando um novo array com os itens anteriores e o novo item.
   }
 
   return (
     <section className='product-card'>
        
-      <img src={thumbnail.replace(/\w\.jpg/gi, 'W.jpg')} alt="product" className='card__image' /> {/*Para melhorar a qualidade da imagem */}
+      <img src={thumbnail.replace(/\w\.jpg/gi, 'W.jpg')} alt="product" className='card__image' /> {/*Para melhorar a qualidade da imagem e 
+                                                                                                     expressão regular para modificar o nome do arquivo da imagem*/}
       
       <div className="card__infos">
         <h2 className='card__price'>{formatCurrency(price)}</h2>
