3daabbd4485c8b6b6e8ab8d7ae7ec7a699a8283c
Lucas <pilatilucas@gmail.com>
2023-12-04 13:35:58 -0300

    Update Cart.jsx

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,20 +5,24 @@ import AppContext from '../../context/AppContext'
 import formatCurrency from '../../utils/formatCurrency'
 
 
-const Cart = () => {
+const Cart = () => {//Responsável por exibir o carrinho e o preço total. Usando o contexto fornecido pelo `AppContext`
 
-  const { cartItems, isCartVisible } = useContext(AppContext);
+  const { cartItems, isCartVisible } = useContext(AppContext);//Usa o useContext para acessar as variáveis de estado cartItems e isCartVisible
 
-  const totalPrice = cartItems.reduce((acc, item) => item.price + acc, 0);
-
-
+  const totalPrice = cartItems.reduce((acc, item) => item.price + acc, 0);//O cartItem é o preço dos itens do carrinho
+        //O  `reduce((acc, item) => item.price + acc, 0)`  É um metodo de arrya no JS. Que ele executa a função callback em cada elemento.
+         //A função callback rebe os 2 argumentos `acc(acumulador)` soma o preço do item atual ao acumulador
+         //`item` Representa cada item do array cartItems durante a iteração.
+         //item.price + acc: Está somando o preço do item atual ao acumulador.
+        // 0: É o valor inicial do acumulador (acc). Neste caso, a função reduce começa a calcular o acumulador a partir do valor zero.  
+                                                
   return (
-    <section className={`cart ${isCartVisible ? 'cart--active' : ''}`}>
+    <section className={`cart ${isCartVisible ? 'cart--active' : ''}`}>//se isCartVisible for verdadeiro adiciona a classe cart--active
       <div className="cart-items">
         { cartItems.map((cartItem) => <CartItem key={cartItem.id} data={cartItem} />) }
       </div>
 
-      <div className="cart-resume">{formatCurrency(totalPrice, 'BRL')}</div>
+      <div className="cart-resume">{formatCurrency(totalPrice)}</div>
     </section>
   )
 }
